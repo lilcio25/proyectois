4d4adcc89b97552a562f379c21d5a8014bc0dbcb
lilcio25 <claudio.acevedo@alu.ucm.cl>
2023-10-04 21:06:09 -0300

    previo migracion

--- proyectois/urls.py
+++ proyectois/urls.py
@@ -7,8 +7,16 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/',views.login, name ='login'),
-    path('home/',views.home, name = 'home'),
     path('login/',auth_views.LogoutView.as_view(), name='login'),
+    path('home/',views.home, name = 'home'),
+    path('passlost/',views.passlost, name = 'passlost'),
+    path('shop/',views.shop, name = 'shop'),
+    path('activos/',views.activos, name = 'activos'),
+    path('pasados/',views.pasados, name ='pasados'),
+    path('config/',views.config, name ='config'),
+    path('perfil/',views.perfil, name ='perfil'),
+
+
 ]
 
 
--- proyectoweb/views.py
+++ proyectoweb/views.py
@@ -6,7 +6,24 @@ from django.shortcuts import render, redirect
 def login (request):
 	return render(request,"proyectoweb/login.html")
 
-
-
 def home (request):
 	return render(request,"proyectoweb/home.html")
+
+def passlost(request):
+	return render(request,"proyectoweb/passlost.html")
+
+def shop(request):
+	return render(request,"proyectoweb/shop.html")
+
+def config(request):
+	return render(request,"proyectoweb/config.html")
+
+def activos(request):
+	return render(request,"proyectoweb/activos.html")
+
+def pasados(request):
+	return render(request,"proyectoweb/pasados.html")
+
+def perfil(request):
+	return render(request,"proyectoweb/perfil.html")
+
